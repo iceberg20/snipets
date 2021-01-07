0fff7007a98a7ec5937fdce9ead779d35558a96c
iceberg20 <italoberg@yahoo.com.br>
2021-01-07 19:00:26 -0300

    add snipets

---
+++ JUnit 5.java
@@ -0,0 +1,12 @@
+@AutoConfigureMockMvc
+@SpringBootTest(classes = LoggingApplication.class)
+@ExtendWith(SpringExtension.class)
+
+import br.ufrn.imd.smartmetropolis.logging.LoggingApplication;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+public class TesteControllerTest {
+}
+
