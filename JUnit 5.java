9a29742f0f8a0e74706f5eb01425a7c6f8e13f97
Ítalo Berg Marques Souza dos Reis <italoberg@yahoo.com.br>
2021-01-07 19:04:33 -0300

    Update JUnit 5.java

--- JUnit 5.java
+++ JUnit 5.java
@@ -10,3 +10,24 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 public class TesteControllerTest {
 }
 
+//// pom.xml
+            <exclusions>
+                <exclusion>
+                    <groupId>junit</groupId>
+                    <artifactId>junit</artifactId>
+                </exclusion>
+            </exclusions>
+        </dependency>
+        <dependency>
+            <groupId>org.junit.jupiter</groupId>
+            <artifactId>junit-jupiter-api</artifactId>
+            <version>5.3.2</version>
+            <scope>test</scope>
+        </dependency>
+        <dependency>
+            <groupId>org.junit.jupiter</groupId>
+            <artifactId>junit-jupiter-engine</artifactId>
+            <version>5.3.2</version>
+            <scope>test</scope>
+        </dependency>
+
